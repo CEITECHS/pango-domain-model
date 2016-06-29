41fb2b54b64a8d9c47378e161e6c4122406c546d
Iddy Magohe <iddyiam@gmail.com>
2016-06-28 21:06:59 -0400

    customer class

--- src/main/java/com/ceitechs/domain/Customer.java
+++ src/main/java/com/ceitechs/domain/Customer.java
@@ -1,7 +1,30 @@
 package com.ceitechs.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
- * Created by iddymagohe on 6/28/16.
+ * @author iddymagohe
+ * @since 0.1
  */
+
+@Getter
+@Setter
 public class Customer {
+
+    private String customerId;
+
+    private String firstName;
+    private String lastName;
+
+    private String phoneNumber;
+    private String emailAddress;
+
+    private String addressLine1;
+    private String addressLine2;
+
+    private String City;
+    private String State;
+    private String zip;
+    private  String country;
 }
