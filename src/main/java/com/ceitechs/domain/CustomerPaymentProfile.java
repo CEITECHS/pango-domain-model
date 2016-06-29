da8319ee2ecd6e165ac8865aa6a919e3b069af94
Iddy Magohe <iddyiam@gmail.com>
2016-06-28 22:24:09 -0400

    PaymentProfile or BankProfile

---
+++ src/main/java/com/ceitechs/domain/CustomerPaymentProfile.java
@@ -0,0 +1,21 @@
+package com.ceitechs.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author iddymagohe
+ * @since 0.1
+ */
+
+@Getter
+@Setter
+public class CustomerPaymentProfile {
+    private String paymentProfileId;
+    private Customer customer;
+    private String accountName;
+    private String routingNumber;
+    private String accountNumber;
+    private boolean defaultPaymentProfile;
+}
